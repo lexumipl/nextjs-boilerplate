180d61272bb4c2ad97019108dec10f54029ca00c
lexumipl <lexumi.pl@gmail.com>
2025-10-07 19:10:17 +0200

    Delete app/export default function Page() {   /app/sign-in/[[...sign-in]] directory

--- app/export default function Page() {   /app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  // leniwa dynamiczna importacja, gdyby coś nie chciało się zbudować
-  const SignIn = require("@clerk/nextjs").SignIn;
-  return (
-    <main style={{ maxWidth: 420, margin: "80px auto", padding: 16 }}>
-      <SignIn />
-    </main>
-  );
-}
